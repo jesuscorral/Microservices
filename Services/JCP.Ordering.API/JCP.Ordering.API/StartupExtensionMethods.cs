57481ea8a8bf7fe7f6cf3370c65875c3e687b4d6
corralperez <corralperez@gmail.com>
2020-10-02 17:12:37 +0200

    Create Ordering API project and install swagger

---
+++ Services/JCP.Ordering.API/JCP.Ordering.API/StartupExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace JCP.Ordering.API
+{
+    public static class StartupExtensionMethods
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services) {
+            return services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Version = "v1",
+                    Title = "Microservices-example",
+                });
+            });
+        }
+    }
+}
